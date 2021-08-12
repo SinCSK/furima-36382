fb667240eda284b8649b78df59d49b98333c47b7
SinCSK <otomeza.man01@gmail.com>
2021-08-12 12:04:08 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  VALID_ZENKAKU_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
+  VALID_KATAKANA_REGEX = /\A[ァ-ヶー－]+\z/
+  VALID_PASSWORD_REGEX =/\A(?=.*?[a-zA-Z])(?=.*?[\d])\w{6,100}\z/
+
+  validates :nickname, presence: true
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+
+  with_options presence: true, format: { with: VALID_ZENKAKU_REGEX } do
+    validates :last_name, presence: true
+    validates :first_name, presence: true
+  end
+
+  with_options presence: true, format: { with: VALID_KATAKANA_REGEX } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+  
+  validates :birth_date, presence: true
+
 end
