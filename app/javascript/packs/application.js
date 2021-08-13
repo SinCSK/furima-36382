8b79fbb07d5f2c2f44739af80b4f95327343781d
SinCSK <otomeza.man01@gmail.com>
2021-08-13 20:38:54 +0900

    商品登録画面の実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 // require("turbolinks").start() //JavaScriptが読み込まれなくなるため、コメントアウト
 require("@rails/activestorage").start()
 require("channels")
+require("../item_price");
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,25 @@
 class Item < ApplicationRecord
-  validates :item_name, presence: true
-  validates :item_show, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :shipping_charge_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :day_to_ship_id, presence: true
-  validates :price, presence: true
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  with_options presence: true do
+    validates :item_name, presence: true
+    validates :item_show, presence: true
+    validates :price, presence: true
+  end
 
   belongs_to :user
   has_one_attached :image
+
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :day_to_ship
+
+  with_options numericality: { other_than: 1 , message: "can't be blank"} do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_charge_id
+    validates :prefecture_id
+    validates :day_to_ship_id
+  end
 end
