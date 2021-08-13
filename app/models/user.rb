52feb534e2f42ecaf33a9b929042988f51a763b3
SinCSK <otomeza.man01@gmail.com>
2021-08-13 12:09:56 +0900

    レビューバック３回目

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,8 @@ class User < ApplicationRecord
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: ". Include both letters and numbers"}
 
   with_options presence: true, format: { with: VALID_ZENKAKU_REGEX, message: ". Input full-width characters"} do
-    validates :last_name, presence: true
-    validates :first_name, presence: true
+    validates :last_name
+    validates :first_name
   end
 
   with_options presence: true, format: { with: VALID_KATAKANA_REGEX, message: ". Input full-width katakana characters"} do
