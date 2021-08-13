e303b4e65f2849a8acabc872b9aadb5da62faa41
SinCSK <otomeza.man01@gmail.com>
2021-08-13 14:49:34 +0900

    バリデーションとアソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  validates :item_name, presence: true
+  validates :item_show, precence: true
+  validates :category_id, precence: true
+  validates :condition_id, precence: true
+  validates :shipping_charge_id, precence: true
+  validates :prefecture_id, precence: true
+  validates :day_to_ship_id, precence: true
+  validates :price, precence: true
+
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,6 @@ class User < ApplicationRecord
   
   validates :birth_date, presence: true
 
+  has_many :items
+
 end
