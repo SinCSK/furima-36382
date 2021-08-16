7b69cbdf2c9aa32826536d4ca207e608a1ca5dcf
SinCSK <otomeza.man01@gmail.com>
2021-08-16 15:38:54 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,3 +6,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :birth_date, :first_name, :last_name, :first_name_kana, :last_name_kana])
   end
 end
+ 
