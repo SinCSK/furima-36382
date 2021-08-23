d49de3ac56867e45322e70df4c00bd7ceedcfb92
SinCSK <otomeza.man01@gmail.com>
2021-08-23 16:39:25 +0900

    shipping_addressコントローラーの修正

--- app/controllers/shipping_addresses_controller.rb
+++ app/controllers/shipping_addresses_controller.rb
@@ -1,4 +1,5 @@
-class ShippingAddressesController< ActionController::Base
+class ShippingAddressesController< ApplicationController
+  before_action :set_item, only: [:index, :create]
   def index
     @purchase = PurchaseForm.new
   end
@@ -7,6 +8,7 @@ class ShippingAddressesController< ActionController::Base
     @purchase = PurchaseForm.new(purchase_params)
     if @purchase.valid?
       @purchase.save
+      redirect_to root_path
     else
       render :index
     end
@@ -16,5 +18,9 @@ class ShippingAddressesController< ActionController::Base
 
   def purchase_params
     params.require(:purchase_form).permit(:card_number, :card_exp_month, :card_exp_year, :card_cvc, :postal_code, :city, :address, :building, :phone_number, :prefecture_id).merge(user_id: current_user.id,item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
