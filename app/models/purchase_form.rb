2b7c5b072e9639e914ad251748b193bc0a7f7f7d
SinCSK <otomeza.man01@gmail.com>
2021-08-24 09:42:30 +0900

    バリデーションの変更

--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -1,13 +1,10 @@
 class PurchaseForm
   include ActiveModel::Model
   attr_accessor :card_number, :card_exp_month, :card_exp_year, :card_cvc,
-                :postal_code, :city, :address, :building, :phone_number, :prefecture_id, :user_id, :item_id
+                :postal_code, :city, :address, :building, :phone_number, :prefecture_id, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :card_number
-    validates :card_exp_month
-    validates :card_exp_year
-    validates :card_cvc
+    validates :token
 
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :city
